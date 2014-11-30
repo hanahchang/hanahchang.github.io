d03c19adeb5ddf0c2e324ab7c8c2a298ed1782db
hanahchang <hanahjeanchang@gmail.com>
2014-11-30 13:33:40 -0800

    jquery

---
+++ assets/js/file.js
@@ -0,0 +1,16 @@
+$( document ).ready(function() {
+
+    $( "#blog" ).click(function() {
+ 
+     	alert( "This page is currently under construction" );
+
+	$( "#photos" ).click(function() {
+ 
+     	alert( "This page is currently under construction" );
+     	
+    });
+
+
+ 
+   
+ 
