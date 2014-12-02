67bcdae1fe3944a05addf5f47120e7d5c6444621
hanahchang <hanahjeanchang@gmail.com>
2014-12-02 04:09:27 -0800

    new

--- assets/js/file.js
+++ assets/js/file.js
@@ -3,14 +3,35 @@ $( document ).ready(function() {
     $( "#blog" ).click(function() {
  
      	alert( "This page is currently under construction" );
-
+ });
 	$( "#photos" ).click(function() {
  
      	alert( "This page is currently under construction" );
      	
-    });
+  
+});
+	$( ".name" ).hover(
+  function() {
+    $( ".name" ).css( "font-weight", "900" );
+  }, function() {
+    $( ".name" ).css( "font-weight", "200" );
+  }
+);
 
+  $("#button").click(function() {
+    if ($("#secondparagraph").is(":visible")) {
+      $("#secondparagraph").slideUp();
+      $("#button-text").text("See More");
+    }
 
+    else {
+      $("#secondparagraph").slideDown();
+      $("#button-text").text("See Less");
+
+    }
+  });
+
+});
  
    
  
