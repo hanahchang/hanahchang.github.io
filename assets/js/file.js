4eebea4827b93f86e6821d9baa56892df5992f3b
hanahchang <hanahjeanchang@gmail.com>
2014-12-02 04:22:52 -0800

    final im crying

--- assets/js/file.js
+++ assets/js/file.js
@@ -2,11 +2,11 @@ $( document ).ready(function() {
 
     $( "#blog" ).click(function() {
  
-     	alert( "This page is currently under construction" );
+     	alert( "This page is currently under construction." );
  });
 	$( "#photos" ).click(function() {
  
-     	alert( "This page is currently under construction" );
+     	alert( "This page is currently under construction." );
      	
   
 });
